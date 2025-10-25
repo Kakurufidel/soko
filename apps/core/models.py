e84265260432660215f55aaaf187f968273ac5cc
Fidel kakuru <132678690+Kakurufidel@users.noreply.github.com>
2025-10-26 01:47:52 +0200

    migrations

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
 
 class BaseModel(models.Model):
     """Modèle abstrait avec des champs communs pour toutes les entités."""
